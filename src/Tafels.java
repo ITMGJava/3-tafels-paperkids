c50e00dee3e2d5259df59e6c7cf928650b199d27
Petra Manders <paperkids@hotmail.com>
2020-09-23 18:22:14 +0200

    3

--- src/Tafels.java
+++ src/Tafels.java
@@ -1,2 +1,16 @@
 public class Tafels {
-}
+    public static void main(String[] args) {
+        var getal = 8;
+        var uitkomst1 = 0;
+
+        uitkomst1 = 1 * getal;
+
+        String regel1 = "1 x ";getal
+        String regel2 = " = ";uitkomst1
+
+        System.out.println(regel1);
+
+    }
+
+
+    }
