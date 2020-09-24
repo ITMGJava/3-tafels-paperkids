e6da9e76415f65f78d79f4cc1437fe5b118a8b57
Petra Manders <paperkids@hotmail.com>
2020-09-24 10:57:59 +0200

    aanpassing finish

--- src/Tafels.java
+++ src/Tafels.java
@@ -1,16 +1,27 @@
 public class Tafels {
     public static void main(String[] args) {
-        String getal = 8;
-        int uitkomst1 = getal;
-        var uitkomst2 = 2 * getal;
-        var uitkomst3 = 3 * getal;
+        var  x = 8;
+        var uitkomst1 = x*1;
+        var uitkomst2 = x*2;
+        var uitkomst3 = x*3;
+        var uitkomst4 = x*4;
+        var uitkomst5 = x*5;
+        var uitkomst6 = x*6;
+        var uitkomst7 = x*7;
+        var uitkomst8 = x*8;
+        var uitkomst9 = x*9;
+        var uitkomst10 = x*10;
 
-        System.out.println ("1") ;
-        "1x" "+" (getal) = uitkomst1;
-        System.out.println("2");
-        "2x" + getal; = uitkomst2
-
-
+        System.out.println("1 x 8 ="+ uitkomst1);
+        System.out.println("2 x 8 =" + uitkomst2);
+        System.out.println("3 x 8 =" + uitkomst3);
+        System.out.println("4 x 8 =" + uitkomst4);
+        System.out.println("5 x 8 =" + uitkomst5);
+        System.out.println("6 x 8 =" + uitkomst6);
+        System.out.println("7 x 8 =" + uitkomst7);
+        System.out.println("8 x 8 =" + uitkomst8);
+        System.out.println("9 x 8 =" + uitkomst9);
+        System.out.println("10 x 8 =" + uitkomst10);
 
     }
 
